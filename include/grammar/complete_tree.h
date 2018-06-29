48042cb479fbb2e31039c93faceca433b4ffa261
Dustin Cobas Batista <dustin.cobas@gmail.com>
2018-06-29 13:19:05 -0400

    Refactor tree balancer

--- include/grammar/complete_tree.h
+++ include/grammar/complete_tree.h
@@ -6,43 +6,69 @@
 #define GRAMMAR_COMPLETE_TREE_H
 
 #include <vector>
-#include <iostream>
-#include <bitset>
 
 
 namespace grammar {
 
+/**
+ * Tree Node
+ *
+ * Used in BalanceTreeByWeight to represent the nodes of tree.
+ * @tparam Weight
+ */
 template<typename Weight>
-struct GrammarTreeNode {
-  int id;
-  Weight weight;
-  std::pair<int, int> cover;
-  std::pair<int, int> children;
-  std::pair<Weight, Weight> children_weight;
-};
+struct TreeNode;
 
 
-template<typename II, typename Output, typename GetWeight, typename CombineWeight>
-void BalanceTreeByWeight(II _begin, II _end, Output &_out, GetWeight _get_weight, CombineWeight _combine_weight) {
-  using Node = GrammarTreeNode<decltype(_get_weight(*_begin))>;
+/**
+ * Default Tree Nodes Comparator
+ */
+class CompareTreeNode;
 
-  auto minor_weight = [](const Node &_n1, const Node &_n2) -> bool {
-    return _n1.weight > _n2.weight
-        || (_n1.weight == _n2.weight && std::min(_n1.children_weight.first, _n1.children_weight.second)
-            > std::min(_n2.children_weight.first, _n2.children_weight.second));
-  };
+
+/**
+ * Default Combine Weight of Subtrees
+ */
+class TreeHeight;
+
+
+/**
+ * Takes a sequence of leaves and completes the binary tree using the given heuristic. The new nodes are reported using _out object.
+ *
+ * @tparam II Input iterator
+ * @tparam Output Reporter
+ * @tparam GetWeight Functor to calculate the weight of input nodes
+ * @tparam CombineWeights Functor to combine weights of subtrees
+ * @tparam CompareNodes Functor to compare nodes
+ *
+ * @param _begin
+ * @param _end
+ * @param _out
+ * @param _get_weight
+ * @param _combine_weight
+ * @param _compare_nodes
+ */
+template<typename II, typename Output, typename GetWeight, typename CombineWeights = TreeHeight, typename CompareNodes = CompareTreeNode>
+void BalanceTreeByWeight(II _begin,
+                         II _end,
+                         Output &_out,
+                         const GetWeight &_get_weight,
+                         const CombineWeights &_combine_weight = CombineWeights(),
+                         const CompareNodes &_compare_nodes = CompareNodes()) {
+  using Node = TreeNode<decltype(_get_weight(*_begin))>;
 
   std::vector<Node> nodes;
-  int length = 0;
+  std::size_t length = 0;
+  // Add leaves
   for (auto it = _begin; it != _end; ++it, ++length) {
-    Node leaf = {length, _get_weight(*it), {length, length}, {-1, -1}, {0, 0}};
-    nodes.emplace_back(leaf);
+    nodes.emplace_back(Node{length, _get_weight(*it), {length, length}, {-1, -1}, {0, 0}});
   }
 
   std::vector<Node> heap;
   heap.reserve(length * 3);
 
-  int id = length;
+  auto id = length;
+  // Add first level nodes
   for (int i = 1; i < length; ++i) {
     Node &left = nodes[i - 1];
     Node &right = nodes[i];
@@ -54,22 +80,26 @@ void BalanceTreeByWeight(II _begin, II _end, Output &_out, GetWeight _get_weight
                            {left.weight, right.weight}});
   }
 
-  std::make_heap(heap.begin(), heap.end(), minor_weight);
+  // Create heap
+  std::make_heap(heap.begin(), heap.end(), _compare_nodes);
 
   for (int j = 1; j < length; ++j) {
     Node node;
 
+    // Extract lighter subtree
     do {
-      std::pop_heap(heap.begin(), heap.end(), minor_weight);
+      std::pop_heap(heap.begin(), heap.end(), _compare_nodes);
       node = heap.back();
       heap.pop_back();
     } while (nodes[node.children.first].id >= node.id || nodes[node.children.second].id >= node.id);
 
     node.id = id;
+    // Report subtree root
     _out(node.id, nodes[node.children.first].id, nodes[node.children.second].id, node.weight);
     nodes[node.cover.first] = nodes[node.cover.second] = node;
 
     ++id;
+    // Add new possible subtrees to the heap
     if (node.cover.first > 0) {
       const auto &left = nodes[node.cover.first - 1];
       heap.emplace_back(Node{id,
@@ -77,7 +107,7 @@ void BalanceTreeByWeight(II _begin, II _end, Output &_out, GetWeight _get_weight
                              {left.cover.first, node.cover.second},
                              {node.cover.first - 1, node.cover.first},
                              {left.weight, node.weight}});
-      std::push_heap(heap.begin(), heap.end(), minor_weight);
+      std::push_heap(heap.begin(), heap.end(), _compare_nodes);
     }
     if (node.cover.second < length - 1) {
       const auto &right = nodes[node.cover.second + 1];
@@ -86,10 +116,71 @@ void BalanceTreeByWeight(II _begin, II _end, Output &_out, GetWeight _get_weight
                              {node.cover.first, right.cover.second},
                              {node.cover.second, node.cover.second + 1},
                              {node.weight, right.weight}});
-      std::push_heap(heap.begin(), heap.end(), minor_weight);
+      std::push_heap(heap.begin(), heap.end(), _compare_nodes);
     }
   }
 }
+
+
+/**
+ * Tree Node
+ *
+ * Used in BalanceTreeByWeight to represent the nodes of tree.
+ * @tparam Weight
+ */
+template<typename Weight>
+struct TreeNode {
+  std::size_t id;
+  Weight weight;
+  std::pair<std::size_t, std::size_t> cover;
+  std::pair<std::size_t, std::size_t> children;
+  std::pair<Weight, Weight> children_weight;
+};
+
+
+/**
+ * Default Tree Nodes Comparator
+ */
+class CompareTreeNode {
+ public:
+  /**
+   *
+   * @tparam Node
+   * @param _n1
+   * @param _n2
+   * @return true (if _n1 is heavier than _n2) or
+   *              (if both have the same weight and (lighter subtree of _n1 is heavier than lighter subtree of _n2 or
+   *              (both have the same weight and _n1 appears after _n2 (more to the right))))
+   */
+  template<typename Node>
+  bool operator()(const Node &_n1, const Node &_n2) const{
+    decltype(_n1.weight) n1_min_child, n2_min_child;
+    return _n1.weight > _n2.weight
+        || (_n1.weight == _n2.weight
+            && ((n1_min_child = std::min(_n1.children_weight.first, _n1.children_weight.second))
+                > (n2_min_child = std::min(_n2.children_weight.first, _n2.children_weight.second)) ||
+                (n1_min_child == n2_min_child && _n1.children.first > _n2.children.first)));
+  }
+};
+
+
+/**
+ * Default Combine Weight of Subtrees
+ */
+class TreeHeight {
+ public:
+  /**
+   *
+   * @tparam Value
+   * @param _v1
+   * @param _v2
+   * @return height of tree formed by join the given subtrees
+   */
+  template<typename Value>
+  Value operator()(const Value &_v1, const Value &_v2) const{
+    return std::max(_v1, _v2) + 1;
+  }
+};
 
 }
 
--- test/complete_tree_test.cpp
+++ test/complete_tree_test.cpp
@@ -13,7 +13,6 @@ class SimpleWrapper {
   SimpleWrapper(std::vector<std::pair<int, int>> &_result) : result{_result} {}
 
   void operator()(int id, int id_left_child, int id_right_child, int height) {
-    std::cout << id << std::endl;
     result.emplace_back(std::make_pair(id_left_child, id_right_child));
   }
 
@@ -21,33 +20,41 @@ class SimpleWrapper {
   std::vector<std::pair<int, int>> &result;
 };
 
-
 class GetSameValue {
  public:
   template<typename Value>
-  Value operator()(Value _v) {
+  Value operator()(Value _v) const {
     return _v;
   }
 };
 
 
-class TreeHeight {
- public:
-  template<typename Value>
-  Value operator()(Value _v1, Value _v2) {
-    return std::max(_v1, _v2) + 1;
-  }
+class CompleteTreeTF : public ::testing::TestWithParam<std::tuple<std::vector<int>, std::vector<std::pair<int, int>>>> {
 };
 
-
-TEST(CompleteTreeTest, construct) {
-  std::vector<int> leaves = {1, 2, 0, 3, 1, 1, 0, 0, 3};
+TEST_P(CompleteTreeTF, construct) {
+  const auto &leaves = std::get<0>(GetParam());
 
   std::vector<std::pair<int, int>> result;
   SimpleWrapper out(result);
 
-  grammar::BalanceTreeByWeight(leaves.begin(), leaves.end(), out, GetSameValue(), TreeHeight());
+  grammar::BalanceTreeByWeight(leaves.begin(), leaves.end(), out, GetSameValue());
 
-  std::vector<std::pair<int, int>> eresult = {{6, 7}, {4, 5}, {1, 2}, {10, 9}, {0, 11}, {3, 12}, {14, 8}, {13, 15}};
-  EXPECT_EQ(result, eresult);
+  EXPECT_EQ(result, std::get<1>(GetParam()));
 }
+
+INSTANTIATE_TEST_CASE_P(CompleteTreeByHeight,
+                        CompleteTreeTF,
+                        ::testing::Values(
+                            std::make_tuple(std::vector<int>{0},
+                                            std::vector<std::pair<int, int>>{}),
+                            std::make_tuple(std::vector<int>{10, 4},
+                                            std::vector<std::pair<int, int>>{{0, 1}}),
+                            std::make_tuple(std::vector<int>{10, 4, 5},
+                                            std::vector<std::pair<int, int>>{{1, 2}, {0, 3}}),
+                            std::make_tuple(std::vector<int>{0, 0, 0, 0, 0, 0, 0, 0},
+                                            std::vector<std::pair<int, int>>{{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9},
+                                                                             {10, 11}, {12, 13}}),
+                            std::make_tuple(std::vector<int>{1, 2, 0, 3, 1, 1, 0, 0, 3},
+                                            std::vector<std::pair<int, int>>{{6, 7}, {4, 5}, {1, 2}, {10, 9}, {0, 11},
+                                                                             {3, 12}, {14, 8}, {13, 15}})));
