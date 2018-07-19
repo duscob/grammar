a9d4314a94697d36b8567cf1e31dea830a4606a6
Dustin Cobas Batista <dustin.cobas@gmail.com>
2018-07-19 16:01:59 -0400

    Add constructors for SLP

--- test/re_pair_test.cpp
+++ test/re_pair_test.cpp
@@ -14,10 +14,10 @@ struct NonTerminal {
   int length;
 };
 
-void PrintTo(const NonTerminal& nt, ::std::ostream* os) {
+
+void PrintTo(const NonTerminal &nt, ::std::ostream *os) {
   *os << "{" << nt.left << "; " << nt.right << "}:" << nt.length;
 }
-
 
 
 bool operator==(const NonTerminal &_nt1, const NonTerminal &_nt2) {
@@ -86,20 +86,26 @@ TEST_P(RePairEncoderTF, encode) {
 }
 
 
-INSTANTIATE_TEST_CASE_P(RePairEncoder,
-                        RePairEncoderTF,
-                        ::testing::Values(
-                            std::make_tuple(std::vector<int>({4, 3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 1, 2, 2, 1, 1}),
-                                            std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}}),
-                                            std::vector<int>{4, 6, 6, 3, 3, 6, 2, 5, 1}),
-                            std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                            std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3},
-                                                                      {11, 8, 3}, {9, 12, 5}, {14, 13, 8}}),
-                                            std::vector<int>{15, 15}),
-                            std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 9, 10, 11, 12, 13, 14}),
-                                            std::vector<NonTerminal>({{1, 2, 2}}),
-                                            std::vector<int>{15, 3, 4, 5, 6, 7, 8, 15, 9, 10, 11, 12, 13, 14})
-                        ));
+INSTANTIATE_TEST_CASE_P(
+    RePairEncoder,
+    RePairEncoderTF,
+    ::testing::Values(
+        std::make_tuple(std::vector<int>({4, 3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 1, 2, 2, 1, 1}),
+                        std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}}),
+                        std::vector<int>{4, 6, 6, 3, 3, 6, 2, 5, 1}),
+        std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8}),
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3}, {11, 8, 3}, {9, 12, 5},
+                                                  {14, 13, 8}}),
+                        std::vector<int>{15, 15}),
+        std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 9, 10, 11, 12, 13, 14}),
+                        std::vector<NonTerminal>({{1, 2, 2}}),
+                        std::vector<int>{15, 3, 4, 5, 6, 7, 8, 15, 9, 10, 11, 12, 13, 14})/*,
+        std::make_tuple(std::vector<int>({1, 2, 3, 1, 2, 2, 1, 1, 1, 1, 3, 1, 2, 3}),
+                        std::vector<NonTerminal>({{1, 1, 2}, {1, 2, 2}, {5, 3, 3}, {5, 2, 3}, {4, 4, 4}, {6, 7, 6},
+                                                  {9, 8, 10}}),
+                        std::vector<int>{15, 15})*/
+    )
+);
 
 
 class RePairEncoderInChomskyNFTF : public ::testing::TestWithParam<std::tuple<std::vector<int>,
@@ -141,32 +147,32 @@ TEST_P(RePairEncoderInChomskyNFTF, encode_slp) {
 }
 
 
-INSTANTIATE_TEST_CASE_P(RePairEncoder,
-                        RePairEncoderInChomskyNFTF,
-                        ::testing::Values(
-                            std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                            std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3},
-                                                                      {11, 8, 3}, {9, 12, 5}, {14, 13, 8},
-                                                                      {15, 15, 16}})),
-                            std::make_tuple(std::vector<int>({4, 3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 1, 2, 2, 1, 1}),
-                                            std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}, {3, 3, 2}, {2, 5, 3},
-                                                                      {4, 6, 4}, {8, 1, 4}, {6, 7, 5}, {11, 6, 8},
-                                                                      {9, 12, 12}, {13, 10, 16}})),
-                            std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8}),
-                                            std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {5, 6, 2}, {7, 8, 2},
-                                                                      {9, 10, 4}, {11, 12, 4}, {13, 14, 8}})),
-                            std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 9, 10, 11, 12, 13, 14}),
-                                            std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {5, 6, 2}, {7, 8, 2},
-                                                                      {9, 10, 2}, {11, 12, 2}, {13, 14, 2},
-                                                                      {15, 16, 4}, {17, 18, 4}, {15, 19, 4},
-                                                                      {20, 21, 4}, {22, 23, 8}, {24, 25, 8},
-                                                                      {26, 27, 16}})),
-                            std::make_tuple(std::vector<int>({8, 3, 6, 8, 3}),
-                                            std::vector<NonTerminal>({{8, 3, 2}, {9, 6, 3}, {10, 9, 5}})),
-                            std::make_tuple(std::vector<int>({1, 2, 3, 1, 2, 2, 1, 1, 1, 1}),
-                                            std::vector<NonTerminal>({{1, 1, 2}, {1, 2, 2}, {5, 3, 3}, {5, 2, 3},
-                                                                      {4, 4, 4}, {6, 7, 6}, {9, 8, 10}}))
-                        )
+INSTANTIATE_TEST_CASE_P(
+    RePairEncoder,
+    RePairEncoderInChomskyNFTF,
+    ::testing::Values(
+        std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8}),
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3}, {11, 8, 3}, {9, 12, 5},
+                                                  {14, 13, 8}, {15, 15, 16}})),
+        std::make_tuple(std::vector<int>({4, 3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 1, 2, 2, 1, 1}),
+                        std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}, {3, 3, 2}, {2, 5, 3}, {4, 6, 4}, {8, 1, 4},
+                                                  {6, 7, 5}, {11, 6, 8}, {9, 12, 12}, {13, 10, 16}})),
+        std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8}),
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {5, 6, 2}, {7, 8, 2}, {9, 10, 4}, {11, 12, 4},
+                                                  {13, 14, 8}})),
+        std::make_tuple(std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 9, 10, 11, 12, 13, 14}),
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {5, 6, 2}, {7, 8, 2}, {9, 10, 2}, {11, 12, 2},
+                                                  {13, 14, 2}, {15, 16, 4}, {17, 18, 4}, {15, 19, 4}, {20, 21, 4},
+                                                  {22, 23, 8}, {24, 25, 8}, {26, 27, 16}})),
+        std::make_tuple(std::vector<int>({8, 3, 6, 8, 3}),
+                        std::vector<NonTerminal>({{8, 3, 2}, {9, 6, 3}, {10, 9, 5}})),
+        std::make_tuple(std::vector<int>({1, 2, 3, 1, 2, 2, 1, 1, 1, 1}),
+                        std::vector<NonTerminal>({{1, 1, 2}, {1, 2, 2}, {5, 3, 3}, {5, 2, 3}, {4, 4, 4}, {6, 7, 6},
+                                                  {9, 8, 10}}))/*,
+        std::make_tuple(std::vector<int>({1, 2, 3, 1, 2, 2, 1, 1, 1, 1, 3, 1, 2, 3}),
+                        std::vector<NonTerminal>({{1, 1, 2}, {1, 2, 2}, {5, 3, 3}, {5, 2, 3}, {4, 4, 4}, {6, 7, 6},
+                                                  {9, 8, 10}}))*/
+    )
 );
 
 
