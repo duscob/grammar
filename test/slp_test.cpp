afbd99416c821349589d5c44ed6f437dca57c118
Dustin Cobas Batista <dustin.cobas@gmail.com>
2018-07-19 20:25:38 -0400

    Add ComputeSpanCover
    This compute the coverage of a span in variables of the SLP.

--- test/slp_test.cpp
+++ test/slp_test.cpp
@@ -12,7 +12,6 @@
 #include "grammar/slp_helper.h"
 #include "grammar/re_pair.h"
 
-
 TEST(SLP, AddRule_Failed) {
 
   auto sigma = 4ul;
@@ -24,10 +23,8 @@ TEST(SLP, AddRule_Failed) {
   EXPECT_EQ(slp.AddRule(1, sigma + 1), sigma + 2);
 }
 
-
 using RightHand = std::pair<std::size_t, std::size_t>;
 using Rules = std::vector<RightHand>;
-
 
 class SLP_TF : public ::testing::TestWithParam<std::tuple<std::shared_ptr<grammar::SLPInterface>,
                                                           std::tuple<std::size_t, Rules>>> {
@@ -59,14 +56,12 @@ class SLP_TF : public ::testing::TestWithParam<std::tuple<std::shared_ptr<gramma
   }
 };
 
-
 TEST_P(SLP_TF, Sigma) {
   auto sigma = GetSigma();
   slp_->Reset(sigma);
 
   EXPECT_EQ(slp_->Sigma(), sigma);
 }
-
 
 TEST_P(SLP_TF, AddRules) {
   auto &sigma = GetSigma();
@@ -81,7 +76,6 @@ TEST_P(SLP_TF, AddRules) {
 
   EXPECT_EQ(slp_->Variables(), sigma + rules.size());
 }
-
 
 TEST_P(SLP_TF, Access) {
   auto &sigma = GetSigma();
@@ -102,7 +96,6 @@ TEST_P(SLP_TF, Access) {
   EXPECT_ANY_THROW((*slp_)[sigma + rules.size() + 1]);
 }
 
-
 TEST_P(SLP_TF, IsTerminal) {
   auto &sigma = GetSigma();
 
@@ -113,7 +106,6 @@ TEST_P(SLP_TF, IsTerminal) {
   EXPECT_FALSE(slp_->IsTerminal(sigma + 1));
   EXPECT_FALSE(slp_->IsTerminal(sigma + 2));
 }
-
 
 TEST_P(SLP_TF, Span) {
   auto &sigma = GetSigma();
@@ -141,7 +133,6 @@ TEST_P(SLP_TF, Span) {
   }
 }
 
-
 TEST_P(SLP_TF, SpanLength) {
   auto &sigma = GetSigma();
   auto &rules = GetRules();
@@ -163,7 +154,6 @@ TEST_P(SLP_TF, SpanLength) {
   }
 }
 
-
 TEST_P(SLP_TF, Reset) {
   auto sigma = GetSigma();
   auto &rules = GetRules();
@@ -175,7 +165,6 @@ TEST_P(SLP_TF, Reset) {
   EXPECT_EQ(slp_->Sigma(), sigma + 5);
   EXPECT_EQ(slp_->Variables(), sigma + 5);
 }
-
 
 INSTANTIATE_TEST_CASE_P(
     SLP,
@@ -191,8 +180,6 @@ INSTANTIATE_TEST_CASE_P(
         )
     )
 );
-
-
 
 class SLPWithMetadata_TF : public ::testing::TestWithParam<std::tuple<std::size_t, Rules>> {
  protected:
@@ -211,7 +198,6 @@ class SLPWithMetadata_TF : public ::testing::TestWithParam<std::tuple<std::size_
   }
 };
 
-
 TEST_P(SLPWithMetadata_TF, PTS) {
   slp_.ComputeMetadata();
 
@@ -227,7 +213,6 @@ TEST_P(SLPWithMetadata_TF, PTS) {
   }
 }
 
-
 INSTANTIATE_TEST_CASE_P(
     SLP,
     SLPWithMetadata_TF,
@@ -238,8 +223,7 @@ INSTANTIATE_TEST_CASE_P(
     )
 );
 
-
-template <typename T>
+template<typename T>
 class SLPGeneric_TF : public ::testing::Test {
 };
 
@@ -263,7 +247,6 @@ TYPED_TEST(SLPGeneric_TF, construct) {
 //    EXPECT_EQ(slp.SpanLength(slp.Sigma() + i + 1), rules[i].length);
   }
 }
-
 
 TEST(SLPGeneric_TF, compute) {
 //  auto sigma = 3ul;
@@ -293,3 +276,126 @@ TEST(SLPGeneric_TF, compute) {
     EXPECT_EQ(slp.GetData(i), span);
   }
 }
+
+using Span = std::pair<std::size_t, std::size_t>;
+using SpanCover = std::vector<std::size_t>;
+
+class SLPSpanCover_TF : public ::testing::TestWithParam<std::tuple<std::size_t, Rules, Span, SpanCover>> {
+ protected:
+  grammar::SLPWithMetadata<grammar::PTS> slp_{0};
+
+// Sets up the test fixture.
+  void SetUp() override {
+    auto &sigma = std::get<0>(GetParam());
+    auto &rules = std::get<1>(GetParam());
+
+    slp_.Reset(sigma);
+
+    for (auto &&rule : rules) {
+      slp_.AddRule(rule.first, rule.second), rule.first;
+    }
+
+    slp_.ComputeMetadata();
+  }
+};
+
+TEST_P(SLPSpanCover_TF, SpanCover) {
+  auto &span = std::get<2>(GetParam());
+
+  SpanCover result;
+  grammar::ComputeSpanCover(slp_, span.first, span.second, back_inserter(result));
+
+  auto &eresult = std::get<3>(GetParam());
+  EXPECT_EQ(result, eresult);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    SLP,
+    SLPSpanCover_TF,
+    ::testing::Values(
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{0, 16},
+            SpanCover{14}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{0, 20},
+            SpanCover{14}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{0, 4},
+            SpanCover{9}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{12, 16},
+            SpanCover{10}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{4, 9},
+            SpanCover{11}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{3, 4},
+            SpanCover{1}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{3, 3},
+            SpanCover{}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{3, 1},
+            SpanCover{}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{3, 1},
+            SpanCover{}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{1, 15},
+            SpanCover{6, 12, 8}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{1, 14},
+            SpanCover{6, 12, 2, 2}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{2, 14},
+            SpanCover{5, 12, 2, 2}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{4, 12},
+            SpanCover{12}
+        ),
+        std::make_tuple(
+            4ul,
+            Rules{{2, 1}, {3, 5}, {3, 3}, {2, 5}, {4, 6}, {8, 1}, {6, 7}, {11, 6}, {9, 12}, {13, 10}},
+            Span{6, 13},
+            SpanCover{1, 7, 6 ,2}
+        )
+    )
+);
