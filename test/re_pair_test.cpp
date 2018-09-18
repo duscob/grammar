04156566fab49d911f4666ff21d6fe5d76e2ca5c
Dustin Cobas Batista <dustin.cobas@gmail.com>
2018-09-18 19:55:12 -0300

    Add tests for RePairReaders

--- test/re_pair_test.cpp
+++ test/re_pair_test.cpp
@@ -4,9 +4,14 @@
 
 #include <gtest/gtest.h>
 
+#include <gflags/gflags.h>
+
 #include "grammar/re_pair.h"
 #include "grammar/slp.h"
 #include "grammar/slp_helper.h"
+
+
+DEFINE_string(datapath, "", "Path of data input.");
 
 
 struct NonTerminal {
@@ -216,4 +221,134 @@ TYPED_TEST(RepairEncoderTypeTF, encodeInCNF) {
   EXPECT_EQ(non_terminals,
             std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3}, {11, 8, 3}, {9, 12, 5}, {14, 13, 8},
                                       {15, 15, 16}}));
+}
+
+
+class RePairReaderTF : public ::testing::TestWithParam<std::tuple<std::string,
+                                                                  int,
+                                                                  std::vector<NonTerminal>,
+                                                                  std::vector<int>>> {
+};
+
+
+TEST_P(RePairReaderTF, read) {
+  int sigma;
+  std::vector<NonTerminal> non_terminals;
+  NonTerminalWrapper report_non_terminals(sigma, non_terminals);
+  std::vector<int> compact_seq;
+  CompactSequenceWrapper report_cseq(compact_seq);
+
+  grammar::RePairReader<false> reader;
+
+  auto datafile = FLAGS_datapath + "/" + std::get<0>(GetParam());
+
+  ASSERT_NO_THROW(reader.Read(datafile, report_non_terminals, report_cseq));
+  EXPECT_EQ(sigma, std::get<1>(GetParam()));
+  EXPECT_EQ(non_terminals, std::get<2>(GetParam()));
+  EXPECT_EQ(compact_seq, std::get<3>(GetParam()));
+}
+
+
+INSTANTIATE_TEST_CASE_P(
+    RePairReader,
+    RePairReaderTF,
+    ::testing::Values(
+        std::make_tuple(std::string("4_3_2_1_3_2_1_3_3_3_2_1_2_2_1_1.int.bin"),
+                        4,
+                        std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}}),
+                        std::vector<int>{4, 6, 6, 3, 3, 6, 2, 5, 1}),
+        std::make_tuple(std::string("1_2_3_4_5_6_7_8_1_2_3_4_5_6_7_8.int.bin"),
+                        8,
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3}, {11, 8, 3}, {9, 12, 5},
+                                                  {14, 13, 8}}),
+                        std::vector<int>{15, 15}),
+        std::make_tuple(std::string("1_2_3_4_5_6_7_8_1_2_9_10_11_12_13_14.int.bin"),
+                        14,
+                        std::vector<NonTerminal>({{1, 2, 2}}),
+                        std::vector<int>{15, 3, 4, 5, 6, 7, 8, 15, 9, 10, 11, 12, 13, 14})
+    )
+);
+
+
+class RePairReaderInChomskyNFTF : public ::testing::TestWithParam<std::tuple<std::string,
+                                                                             int,
+                                                                             std::vector<NonTerminal>>> {
+};
+
+
+TEST_P(RePairReaderInChomskyNFTF, read) {
+  int sigma;
+  std::vector<NonTerminal> non_terminals;
+  NonTerminalWrapper report_non_terminals(sigma, non_terminals);
+  std::vector<int> compact_seq;
+
+  grammar::RePairReader<true> reader;
+
+  auto datafile = FLAGS_datapath + "/" + std::get<0>(GetParam());
+
+  ASSERT_NO_THROW(reader.Read(datafile, report_non_terminals));
+  EXPECT_EQ(sigma, std::get<1>(GetParam()));
+  EXPECT_EQ(non_terminals, std::get<2>(GetParam()));
+}
+
+
+INSTANTIATE_TEST_CASE_P(
+    RePairReader,
+    RePairReaderInChomskyNFTF,
+    ::testing::Values(
+        std::make_tuple(std::string("4_3_2_1_3_2_1_3_3_3_2_1_2_2_1_1.int.bin"),
+                        4,
+                        std::vector<NonTerminal>({{2, 1, 2}, {3, 5, 3}, {3, 3, 2}, {2, 5, 3}, {4, 6, 4}, {8, 1, 4},
+                                                  {6, 7, 5}, {11, 6, 8}, {9, 12, 12}, {13, 10, 16}})),
+        std::make_tuple(std::string("1_2_3_4_5_6_7_8_1_2_3_4_5_6_7_8.int.bin"),
+                        8,
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {6, 7, 2}, {10, 5, 3}, {11, 8, 3}, {9, 12, 5},
+                                                  {14, 13, 8}, {15, 15, 16}})),
+        std::make_tuple(std::string("1_2_3_4_5_6_7_8_1_2_9_10_11_12_13_14.int.bin"),
+                        14,
+                        std::vector<NonTerminal>({{1, 2, 2}, {3, 4, 2}, {5, 6, 2}, {7, 8, 2}, {9, 10, 2}, {11, 12, 2},
+                                                  {13, 14, 2}, {15, 16, 4}, {17, 18, 4}, {15, 19, 4}, {20, 21, 4},
+                                                  {22, 23, 8}, {24, 25, 8}, {26, 27, 16}}))
+    )
+);
+
+
+//TEST(RePair, repair) {
+//  std::string fname = "/home/dcobas/Workspace/PhD/Research/Document_Retrieval/Codes/repair/bal/docs.out";
+//
+//  FILE *Tf, *Rf, *Cf;
+//
+//  struct stat s;
+//  if (stat(fname.c_str(), &s) != 0) {
+//    fprintf(stderr, "Error: cannot stat file %s\n", fname.c_str());
+//    exit(1);
+//  }
+//
+//  int i, len;
+//  len = s.st_size / sizeof(int);
+//  Tf = fopen(fname.c_str(), "r");
+//  if (Tf == NULL) {
+//    fprintf(stderr, "Error: cannot open file %s for reading\n", fname.c_str());
+//    exit(1);
+//  }
+//
+//  int *C = (int *) malloc(len * sizeof(int));
+//  if (fread(C, sizeof(int), len, Tf) != len) {
+//    fprintf(stderr, "Error: cannot read file %s\n", fname.c_str());
+//    exit(1);
+//  }
+//  fclose(Tf);
+//
+//  grammar::RePairBasicEncoder encoder;
+//  encoder.prepare(C, len);
+//  encoder.repair(C, len, [](auto, auto, auto){});
+//}
+
+
+int main(int argc, char **argv) {
+  gflags::AllowCommandLineReparsing();
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
