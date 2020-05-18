333940bdb7d6ed5cc323608c475dfeb8c0b59cfb
Dustin Cobas <dustin.cobas@gmail.com>
2020-05-18 01:39:55 -0400

    Add tools to create Differential SLP

---
+++ tool/definitions.h
@@ -0,0 +1,13 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 5/17/20.
+//
+
+#ifndef GRAMMAR_TOOL_DEFINITIONS_H_
+#define GRAMMAR_TOOL_DEFINITIONS_H_
+
+const char *KEY_GRM_SPAN_SUMS = "grm_sums";
+const char *KEY_GRM_SAMPLE_VALUES = "grm_sample_val";
+const char *KEY_GRM_SAMPLE_POSITIONS = "grm_sample_pos";
+const char *KEY_GRM_SAMPLE_ROOTS_POSITIONS = "grm_sample_root_pos";
+
+#endif //GRAMMAR_TOOL_DEFINITIONS_H_
