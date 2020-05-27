3c013fb4e0e63d9fa797322420fd01ee42f1f7cc
Dustin Cobas <dustin.cobas@gmail.com>
2020-05-27 02:52:43 -0400

    Adapt tools to bigger dataset

--- tool/build_dslp_samples.cpp
+++ tool/build_dslp_samples.cpp
@@ -19,7 +19,6 @@
 
 #include "definitions.h"
 
-
 DEFINE_string(data, "", "Data file. (MANDATORY)");
 DEFINE_int32(max_size, 0, "Maximum size of sampled intervals. (MANDATORY)");
 
@@ -38,70 +37,77 @@ int main(int argc, char **argv) {
 
   sdsl::cache_config config(false, ".", base_name.string());
 
-  grammar::SLP<> slp;
-  std::vector<std::size_t> compact_seq;
-  {
-    grammar::RePairReader<false> re_pair_reader;
-    auto slp_wrapper = grammar::BuildSLPWrapper(slp);
+  if (!sdsl::cache_file_exists(KEY_GRM_SAMPLE_VALUES, config)
+      || !sdsl::cache_file_exists(KEY_GRM_SAMPLE_ROOTS_POSITIONS, config)
+      || !sdsl::cache_file_exists(KEY_GRM_SAMPLE_POSITIONS, config)) {
+    grammar::SLP<> slp;
+    std::vector<std::size_t> compact_seq;
+    {
+      grammar::RePairReader<false> re_pair_reader;
+      auto slp_wrapper = grammar::BuildSLPWrapper(slp);
 
-    auto report_compact_seq = [&compact_seq](const auto &_var) {
-      compact_seq.emplace_back(_var);
+      auto report_compact_seq = [&compact_seq](const auto &_var) {
+        compact_seq.emplace_back(_var);
+      };
+
+      re_pair_reader.Read(datafile.string(), slp_wrapper, report_compact_seq);
+    }
+
+    std::size_t seq_size;
+    uint32_t diff_base_seq;
+    {
+      std::ifstream in(datafile.string() + ".info");
+      in >> seq_size;
+
+      int32_t minimal;
+      in >> minimal;
+      diff_base_seq = minimal < 0 ? std::abs(minimal) : 0;
+    }
+
+    sdsl::int_vector<> span_sums(slp.GetRules().size());
+    sdsl::load_from_cache(span_sums, KEY_GRM_SPAN_SUMS, config);
+
+    uint32_t diff_base_sums;
+    {
+      std::ifstream in(cache_file_name(KEY_GRM_SPAN_SUMS, config) + ".info");
+      int32_t minimal;
+      in >> minimal;
+      diff_base_sums = minimal < 0 ? std::abs(minimal) : 0;
+    }
+
+    auto get_span_sum = [&slp, &diff_base_seq, &span_sums, &diff_base_sums](auto _var) {
+      return slp.IsTerminal(_var) ? (_var - diff_base_seq) : (span_sums[_var - slp.Sigma() - 1] - diff_base_sums);
     };
 
-    re_pair_reader.Read(datafile.string(), slp_wrapper, report_compact_seq);
+    sdsl::bit_vector tmp_sample_pos(seq_size, 0);
+    std::vector<std::size_t> tmp_samples;
+    tmp_samples.reserve(compact_seq.size());
+    std::vector<std::size_t> tmp_sample_roots_pos;
+    tmp_sample_roots_pos.reserve(compact_seq.size());
+    auto report_sample =
+        [&tmp_sample_pos, &tmp_samples, &tmp_sample_roots_pos](auto _seq_pos, auto _sum, auto _root_pos) {
+          tmp_sample_pos[_seq_pos] = 1;
+          tmp_samples.emplace_back(_sum);
+          tmp_sample_roots_pos.emplace_back(_root_pos);
+        };
+
+    grammar::ComputeSamplesOnCompactSequence(compact_seq, slp, get_span_sum, FLAGS_max_size, report_sample);
+
+    sdsl::int_vector<> samples;
+    grammar::Construct(samples, tmp_samples);
+    sdsl::util::bit_compress(samples);
+    sdsl::store_to_cache(samples, KEY_GRM_SAMPLE_VALUES /*+ ("_" + std::to_string(FLAGS_max_size))*/, config);
+
+    sdsl::int_vector<> sample_roots_pos;
+    grammar::Construct(sample_roots_pos, tmp_sample_roots_pos);
+    sdsl::util::bit_compress(sample_roots_pos);
+    sdsl::store_to_cache(sample_roots_pos,
+                         KEY_GRM_SAMPLE_ROOTS_POSITIONS /*+ ("_" + std::to_string(FLAGS_max_size))*/,
+                         config);
+
+    sdsl::sd_vector<> sample_pos(tmp_sample_pos);
+    sdsl::store_to_cache(sample_pos, KEY_GRM_SAMPLE_POSITIONS /*+ ("_" + std::to_string(FLAGS_max_size))*/, config);
   }
-
-  std::size_t seq_size;
-  uint32_t diff_base_seq;
-  {
-    std::ifstream in(datafile.string() + ".info");
-    in >> seq_size;
-
-    int32_t minimal;
-    in >> minimal;
-    diff_base_seq = minimal < 0 ? std::abs(minimal) : 0;
-  }
-
-  sdsl::int_vector<> span_sums(slp.GetRules().size());
-  sdsl::load_from_cache(span_sums, KEY_GRM_SPAN_SUMS, config);
-
-  uint32_t diff_base_sums;
-  {
-    std::ifstream in(cache_file_name(KEY_GRM_SPAN_SUMS, config) + ".info");
-    int32_t minimal;
-    in >> minimal;
-    diff_base_sums = minimal < 0 ? std::abs(minimal) : 0;
-  }
-
-  auto get_span_sum = [&slp, &diff_base_seq, &span_sums, &diff_base_sums](auto _var){
-    return slp.IsTerminal(_var) ? (_var - diff_base_seq) : (span_sums[_var - slp.Sigma() - 1] - diff_base_sums);
-  };
-
-  sdsl::bit_vector tmp_sample_pos(seq_size, 0);
-  std::vector<std::size_t> tmp_samples;
-  tmp_samples.reserve(compact_seq.size());
-  std::vector<std::size_t> tmp_sample_roots_pos;
-  tmp_sample_roots_pos.reserve(compact_seq.size());
-  auto report_sample = [&tmp_sample_pos, &tmp_samples, &tmp_sample_roots_pos](auto _seq_pos, auto _sum, auto _root_pos) {
-    tmp_sample_pos[_seq_pos] = 1;
-    tmp_samples.emplace_back(_sum);
-    tmp_sample_roots_pos.emplace_back(_root_pos);
-  };
-
-  grammar::ComputeSamplesOnCompactSequence(compact_seq, slp, get_span_sum, FLAGS_max_size, report_sample);
-
-  sdsl::int_vector<> samples;
-  grammar::Construct(samples, tmp_samples);
-  sdsl::util::bit_compress(samples);
-  sdsl::store_to_cache(samples, KEY_GRM_SAMPLE_VALUES /*+ ("_" + std::to_string(FLAGS_max_size))*/, config);
-
-  sdsl::int_vector<> sample_roots_pos;
-  grammar::Construct(sample_roots_pos, tmp_sample_roots_pos);
-  sdsl::util::bit_compress(sample_roots_pos);
-  sdsl::store_to_cache(sample_roots_pos, KEY_GRM_SAMPLE_ROOTS_POSITIONS /*+ ("_" + std::to_string(FLAGS_max_size))*/, config);
-
-  sdsl::sd_vector<> sample_pos(tmp_sample_pos);
-  sdsl::store_to_cache(sample_pos, KEY_GRM_SAMPLE_POSITIONS /*+ ("_" + std::to_string(FLAGS_max_size))*/, config);
 
   return 0;
 }
--- tool/build_dslp_span_sums.cpp
+++ tool/build_dslp_span_sums.cpp
@@ -34,43 +34,42 @@ int main(int argc, char **argv) {
 
   boost::filesystem::path datafile = FLAGS_data;
   auto base_name = datafile.stem();
-//  std::cout << datafile << " --- " << base_name << std::endl;
-//  return 0;
+
   sdsl::cache_config config(false, ".", base_name.string());
 
-  grammar::SLP<> slp;
-  std::vector<std::size_t> compact_seq;
-  {
-    grammar::RePairReader<false> re_pair_reader;
-    auto slp_wrapper = grammar::BuildSLPWrapper(slp);
+  if (!sdsl::cache_file_exists(KEY_GRM_SPAN_SUMS, config)) {
+    grammar::SLP<> slp;
+    std::vector<std::size_t> compact_seq;
+    {
+      grammar::RePairReader<false> re_pair_reader;
+      auto slp_wrapper = grammar::BuildSLPWrapper(slp);
 
-    auto report_compact_seq = [&compact_seq](const auto &_var) {
-      compact_seq.emplace_back(_var);
+      auto report_compact_seq = [&compact_seq](const auto &_var) {
+        compact_seq.emplace_back(_var);
+      };
+
+      re_pair_reader.Read(datafile.string(), slp_wrapper, report_compact_seq);
+    }
+
+    uint64_t diff_base;
+    std::size_t seq_size;
+    {
+      std::ifstream in(datafile.string() + ".info");
+      in >> seq_size;
+
+      int64_t minimal;
+      in >> minimal;
+      diff_base = minimal < 0 ? std::abs(minimal) : 0;
+    }
+
+    sdsl::int_vector<> span_sums(slp.GetRules().size());
+//    std::size_t i = 0;
+    auto sigma = slp.Sigma();
+    auto report_sum = [&span_sums, sigma](auto _i, auto _sum) {
+      span_sums[_i - sigma - 1] = _sum;
     };
 
-    re_pair_reader.Read(datafile.string(), slp_wrapper, report_compact_seq);
-  }
-
-  uint32_t diff_base;
-  std::size_t seq_size;
-  {
-    std::ifstream in(datafile.string() + ".info");
-    in >> seq_size;
-
-    int32_t minimal;
-    in >> minimal;
-    diff_base = minimal < 0 ? std::abs(minimal) : 0;
-  }
-//  std::cout << "Diff base: " << diff_base << std::endl;
-
-  sdsl::int_vector<> span_sums(slp.GetRules().size());
-  std::size_t i = 0;
-  auto sigma = slp.Sigma();
-  auto report_sum = [&span_sums, sigma](auto _i, auto _sum) {
-    span_sums[_i - sigma - 1] = _sum;
-  };
-
-  auto minmax = grammar::ComputeSpanSums(slp, diff_base, report_sum);
+    auto minmax = grammar::ComputeSpanSums(slp, diff_base, report_sum);
 
 //  std::cout << "sigma: " << slp.Sigma() << std::endl;
 //  std::cout << "# rules: " << slp.GetRules().size() << std::endl;
@@ -81,14 +80,15 @@ int main(int argc, char **argv) {
 //  auto mm = std::minmax_element(span_sums.begin(), span_sums.end());
 //  std::cout << "minmax new span sum: " << *mm.first << " --- " << *mm.second << std::endl;
 
-  sdsl::util::bit_compress(span_sums);
+    sdsl::util::bit_compress(span_sums);
 
-  sdsl::store_to_cache(span_sums, KEY_GRM_SPAN_SUMS, config);
+    sdsl::store_to_cache(span_sums, KEY_GRM_SPAN_SUMS, config);
 
-  {
-    std::ofstream out(cache_file_name(KEY_GRM_SPAN_SUMS, config) + ".info");
-    out << minmax.first << std::endl;
-    out << minmax.second << std::endl;
+    {
+      std::ofstream out(cache_file_name(KEY_GRM_SPAN_SUMS, config) + ".info");
+      out << minmax.first << std::endl;
+      out << minmax.second << std::endl;
+    }
   }
 
   return 0;
