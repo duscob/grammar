0967fb146ddb92397b9da3a1570d955ef22dec3a
Dustin Cobas <dustin.cobas@gmail.com>
2021-05-19 19:09:18 -0400

    Add tools to expand basic compact grammar representation
    The basic compact grammar representation is described in "Locally Compressed Suffix Arrays. Rodrigo González, Gonzalo Navarro, and Héctor Ferrada. ACM Journal of Experimental Algorithmics 19(1):article 1, 2014".
    Using these tools you can expand a variable of the grammar completely or partially from the beginning.

--- include/grammar/slp_compact.h
+++ include/grammar/slp_compact.h
@@ -111,6 +111,95 @@ auto CreateCompactGrammarTreeWithBP(const TRoots &t_roots, const TSigma &t_sigma
   return std::make_tuple(std::move(tree), std::move(leaves), std::move(nt_id));
 }
 
+template<typename TNode, typename TState, typename TIsTerminal, typename TGetNextLeaf, typename TIsTreeComplete, typename TReport>
+void ExpandInternalNodeCompactSLPFromFront(const TNode &t_root,
+                                           std::size_t &t_length,
+                                           TState t_state,
+                                           TIsTerminal &t_is_terminal,
+                                           TGetNextLeaf &&t_get_next_leaf,
+                                           TIsTreeComplete &t_is_tree_complete,
+                                           TReport &t_report) {
+  auto node = t_root;
+  auto state = t_state;
+  std::remove_reference_t<decltype(std::get<1>(t_get_next_leaf(node, state)))> var;
+
+  do {
+    std::tie(node, var, state) = t_get_next_leaf(node, state);
+
+    if (t_is_terminal(var)) {
+      t_report(var);
+      --t_length;
+    } else {
+      ExpandInternalNodeCompactSLPFromFront(
+          var, t_length, TState(), t_is_terminal, t_get_next_leaf, t_is_tree_complete, t_report);
+    }
+  } while (!t_is_tree_complete(state) && t_length > 0);
+}
+
+template<typename TNode, typename TTree, typename TLeafRank, typename TLeaves, typename TReport>
+void ExpandCompactSLPFromFront(const TNode &t_root,
+                               std::size_t t_length,
+                               std::size_t t_sigma,
+                               const TTree &t_tree,
+                               const TLeafRank &t_leaf_rank,
+                               const TLeaves &t_leaves,
+                               TReport &t_report) {
+  if (t_length == 0) return;
+
+  auto is_terminal = [&t_sigma](auto tt_var) {
+    return tt_var <= t_sigma;
+  };
+
+  if (is_terminal(t_root)) {
+    t_report(t_root);
+    return;
+  } else if (auto root = t_root - t_sigma - 1; t_tree[root] == 0) {
+    t_report(t_leaves[t_leaf_rank(root)]);
+    return;
+  }
+
+  struct State {
+    int excess = 2;
+    bool first_leaf_found = false;
+    int leaf_pos = 0;
+  } state;
+
+  auto get_next_leaf = [&t_sigma, &t_tree, &t_leaf_rank, &t_leaves](auto tt_node, auto tt_state) {
+    tt_node -= (t_sigma + 1);
+
+    while (t_tree[++tt_node] == 1) {
+      ++tt_state.excess;
+    }
+
+    --tt_state.excess;
+    if (!tt_state.first_leaf_found) {
+      tt_state.first_leaf_found = true;
+      tt_state.leaf_pos = t_leaf_rank(tt_node);
+    } else {
+      ++tt_state.leaf_pos;
+    }
+
+    return std::make_tuple(tt_node + t_sigma + 1, t_leaves[tt_state.leaf_pos], tt_state);
+  };
+
+  auto is_tree_complete = [](const auto &tt_state) {
+    return tt_state.excess == 0;
+  };
+
+  ExpandInternalNodeCompactSLPFromFront(
+      t_root, t_length, state, is_terminal, get_next_leaf, is_tree_complete, t_report);
+}
+
+template<typename TNode, typename TTree, typename TLeafRank, typename TLeaves, typename TReport>
+void ExpandCompactSLP(const TNode &t_root,
+                      std::size_t t_sigma,
+                      const TTree &t_tree,
+                      const TLeafRank &t_leaf_rank,
+                      const TLeaves &t_leaves,
+                      TReport &t_report) {
+  ExpandCompactSLPFromFront(
+      t_root, std::numeric_limits<std::size_t>::max(), t_sigma, t_tree, t_leaf_rank, t_leaves, t_report);
+}
 }
 
 #endif //GRAMMAR_SLP_COMPACT_H_
--- test/slp_compact_test.cpp
+++ test/slp_compact_test.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 
+#include <sdsl/bit_vectors.hpp>
+
 #include "grammar/slp_compact.h"
 
 class CreateCompactSLP_TF : public ::testing::TestWithParam<
@@ -135,3 +137,152 @@ INSTANTIATE_TEST_SUITE_P(
     )
 );
 
+class ExpandCompactSLP_TF : public ::testing::TestWithParam<
+    std::tuple<size_t, // Sigma
+               sdsl::bit_vector, // Tree
+               std::vector<size_t>, // Leaves
+               size_t, // Variable
+               std::vector<size_t> // Expansion
+    >
+> {
+};
+
+TEST_P(ExpandCompactSLP_TF, ExpandCompactSLP) {
+  const auto &sigma = std::get<0>(GetParam());
+  const auto &tree = std::get<1>(GetParam());
+  auto rank_leaf = sdsl::bit_vector::rank_0_type(&tree);
+  const auto &leaves = std::get<2>(GetParam());
+  const auto &var = std::get<3>(GetParam());
+
+  std::vector<size_t> expansion;
+  auto report = [&expansion](auto tt_term) {
+    expansion.emplace_back(tt_term);
+  };
+
+  grammar::ExpandCompactSLP(var, sigma, tree, rank_leaf, leaves, report);
+
+  const auto &e_expansion = std::get<4>(GetParam());
+  ASSERT_EQ(e_expansion, expansion);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CompactSLP,
+    ExpandCompactSLP_TF,
+    ::testing::Values(
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            4,
+            std::vector<size_t>{1, 2, 3, 1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            6,
+            std::vector<size_t>{1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 0, 0, 0, 1, 0, 0},
+            std::vector<size_t>{1, 2, 3, 4, 5},
+            9,
+            std::vector<size_t>{1, 2, 3, 1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{2, 3, 1, 3},
+            4,
+            std::vector<size_t>{2, 3, 1, 3}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{2, 3, 1, 3},
+            3,
+            std::vector<size_t>{3}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{2, 3, 1, 3},
+            7,
+            std::vector<size_t>{2})
+    )
+);
+
+class ExpandPartialCompactSLP_TF : public ::testing::TestWithParam<
+    std::tuple<size_t, // Sigma
+               sdsl::bit_vector, // Tree
+               std::vector<size_t>, // Leaves
+               size_t, // Variable
+               size_t, // Length
+               std::vector<size_t> // Expansion
+    >
+> {
+};
+
+TEST_P(ExpandPartialCompactSLP_TF, ExpandCompactSLPFromFront) {
+  const auto &sigma = std::get<0>(GetParam());
+  const auto &tree = std::get<1>(GetParam());
+  auto rank_leaf = sdsl::bit_vector::rank_0_type(&tree);
+  const auto &leaves = std::get<2>(GetParam());
+  const auto &var = std::get<3>(GetParam());
+  const auto &len = std::get<4>(GetParam());
+
+  std::vector<size_t> expansion;
+  auto report = [&expansion](auto tt_term) {
+    expansion.emplace_back(tt_term);
+  };
+
+  grammar::ExpandCompactSLPFromFront(var, len, sigma, tree, rank_leaf, leaves, report);
+
+  const auto &e_expansion = std::get<5>(GetParam());
+  ASSERT_EQ(e_expansion, expansion);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CompactSLP,
+    ExpandPartialCompactSLP_TF,
+    ::testing::Values(
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            4,
+            5,
+            std::vector<size_t>{1, 2, 3, 1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            4,
+            10,
+            std::vector<size_t>{1, 2, 3, 1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            4,
+            2,
+            std::vector<size_t>{1, 2}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{1, 2, 3, 6},
+            4,
+            0,
+            std::vector<size_t>{}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{2, 3, 1, 3},
+            3,
+            1,
+            std::vector<size_t>{3}),
+        std::make_tuple(
+            3,
+            sdsl::bit_vector{1, 1, 1, 0, 0, 0, 0},
+            std::vector<size_t>{2, 3, 1, 3},
+            7,
+            5,
+            std::vector<size_t>{2})
+    )
+);
