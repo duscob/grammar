b521c57adc37c73edab504f6332ee642f6b2becb
Dustin Cobas <dustin.cobas@gmail.com>
2025-10-26 17:20:45 -0400

    Switch to FetchContent for managing CMake dependencies
    Replaced `DownloadProject` with `FetchContent` for managing external dependencies (Google Benchmark, GFlags, SDSL, and Google Test). Updated relevant configurations and removed outdated compatibility logic.

--- cmake/ConfigGBenchmark.cmake
+++ cmake/ConfigGBenchmark.cmake
@@ -1,29 +1,19 @@
-# Adapted from https://github.com/Crascit/DownloadProject/blob/master/CMakeLists.txt
-#
-# CAVEAT: use DownloadProject.cmake
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-if (CMAKE_VERSION VERSION_LESS 3.2)
-    set(UPDATE_DISCONNECTED_IF_AVAILABLE "")
-else ()
-    set(UPDATE_DISCONNECTED_IF_AVAILABLE "UPDATE_DISCONNECTED 1")
-endif ()
+set(ExternalProjectName googlebenchmark)
 
-include(DownloadProject)
-download_project(PROJ googlebenchmark
+include(FetchContent)
+FetchContent_Declare(
+        ${ExternalProjectName}
         GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG v1.9.0
-        ${UPDATE_DISCONNECTED_IF_AVAILABLE})
+        GIT_TAG v1.9.4
+        FIND_PACKAGE_ARGS
+)
 
 #Benchmark
 # If you want to self-test benchmark lib too, turn me ON
-#
 set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "BENCHMARK_ENABLE_TESTING")
 set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "BENCHMARK_ENABLE_GTEST_TESTS")
 
 # Prevent Benchmark installation
 set(BENCHMARK_ENABLE_INSTALL OFF CACHE BOOL "" FORCE)
 
-add_subdirectory(${googlebenchmark_SOURCE_DIR} ${googlebenchmark_BINARY_DIR})
-
-include_directories("${googlebenchmark_SOURCE_DIR}/include")
+FetchContent_MakeAvailable(${ExternalProjectName})
