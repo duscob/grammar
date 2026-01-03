d697dc96cb4fe5db7ff4a4763c75dbbcb81a749b
Dustin Cobas <dustin.cobas@gmail.com>
2026-01-03 16:20:12 -0500

    Improve code style and consistency in `io.h`
    Adjusted spacing around `template` keyword, standardized pointer and reference placement, and reformatted inline comments for readability.

--- include/grammar/io.h
+++ include/grammar/io.h
@@ -5,31 +5,30 @@
 #ifndef GRAMMAR_IO_H
 #define GRAMMAR_IO_H
 
+#include <cstdint>
 #include <iostream>
 #include <utility>
-#include <cstdint>
 
 #include <sdsl/io.hpp>
 
 
 namespace std {
 
-template<typename X, typename Y>
-uint64_t serialize(const std::pair<X, Y> &x,
-                   std::ostream &out,
-                   sdsl::structure_tree_node *v = nullptr,
-                   const std::string &name = "") {
+template <typename X, typename Y>
+uint64_t serialize(const std::pair<X, Y>& x,
+                   std::ostream& out,
+                   sdsl::structure_tree_node* v = nullptr,
+                   const std::string& name = "") {
   return serialize(x.first, out, v, name) + serialize(x.second, out, v, name);
 }
 
-
-template<typename X, typename Y>
-void load(std::pair<X, Y> &x, std::istream &in) {
+template <typename X, typename Y>
+void load(std::pair<X, Y>& x, std::istream& in) {
   using sdsl::load;
   load(x.first, in);
   load(x.second, in);
 }
 
-}
+}  // namespace std
 
-#endif //GRAMMAR_IO_H
+#endif  // GRAMMAR_IO_H
