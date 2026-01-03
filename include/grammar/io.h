fc2a79eea6c30a9877f8091924d726270dcc2441
Dustin Cobas <dustin.cobas@gmail.com>
2026-01-03 16:24:20 -0500

    Add `is_serializable` and `is_loadable` traits for enhanced type safety in `io.h`
    Introduced SFINAE-based traits to check serializability and loadability of types. Updated `serialize` and `load` functions for `std::pair` to use these traits.

--- include/grammar/io.h
+++ include/grammar/io.h
@@ -11,19 +11,40 @@
 
 #include <sdsl/io.hpp>
 
+namespace grammar {
+
+template <typename T, typename = void>
+struct is_serializable : std::false_type {};
+
+template <typename T>
+struct is_serializable<T,
+                       std::void_t<decltype(serialize(std::declval<T>(),
+                                                      std::declval<std::ostream&>(),
+                                                      std::declval<sdsl::structure_tree_node*>(),
+                                                      std::declval<const std::string&>()))>> : std::true_type {};
+
+template <typename T, typename = void>
+struct is_loadable : std::false_type {};
+
+template <typename T>
+struct is_loadable<T, std::void_t<decltype(load(std::declval<T>(), std::declval<std::istream&>()))>> : std::true_type {
+};
+
+}  // namespace grammar
 
 namespace std {
 
 template <typename X, typename Y>
-uint64_t serialize(const std::pair<X, Y>& x,
-                   std::ostream& out,
-                   sdsl::structure_tree_node* v = nullptr,
-                   const std::string& name = "") {
+std::enable_if_t<!grammar::is_serializable<std::pair<X, Y>>::value, uint64_t> serialize(
+    const std::pair<X, Y>& x,
+    std::ostream& out,
+    sdsl::structure_tree_node* v = nullptr,
+    const std::string& name = "") {
   return serialize(x.first, out, v, name) + serialize(x.second, out, v, name);
 }
 
 template <typename X, typename Y>
-void load(std::pair<X, Y>& x, std::istream& in) {
+std::enable_if_t<!grammar::is_loadable<std::pair<X, Y>>::value, void> load(std::pair<X, Y>& x, std::istream& in) {
   using sdsl::load;
   load(x.first, in);
   load(x.second, in);
